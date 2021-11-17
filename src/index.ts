00d2609935d2fa9a34bf248fb18c8a9b20707082
unknown <shoodey@gmail.com>
2021-11-17 03:22:30 +0200

    env var fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
-const token = process.env["token"];
+const token = process.env["TOKEN"];
 
 client.on("ready", () => {
   consola.success("Client is ready!");
