49a792669e84195900dd0149ec4b7456113e4b1a
unknown <shoodey@gmail.com>
2021-11-17 03:05:15 +0200

    initial bot templating

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { Client, Intents } from "discord.js";
+import consola from "consola";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+const token = process.env["token"];
+
+client.on("ready", () => {
+  consola.success("Client is ready!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "ping") {
+    message.reply({ content: "pong!" });
+  }
+});
+
+client.login(token);
